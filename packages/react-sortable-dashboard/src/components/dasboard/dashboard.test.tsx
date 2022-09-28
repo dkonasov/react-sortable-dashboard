b73dcca1ca84b7b50c668ab4fafd5229cdccfc82
Konasov Dmitriy <d.konasov@yandex.ru>
2022-09-28 20:04:02 +0300

    tests: setup tests

--- packages/react-sortable-dashboard/src/components/dasboard/dashboard.test.tsx
+++ packages/react-sortable-dashboard/src/components/dasboard/dashboard.test.tsx
@@ -1,16 +1,22 @@
 import { render, unmountComponentAtNode } from "react-dom";
+import client, { Root } from "react-dom/client";
 import { act } from "react-dom/test-utils";
 import { Dashboard } from "./dashboard";
 
 let container: HTMLDivElement | null = null;
+let root: Root | null;
 
 beforeEach(() => {
   container = document.createElement("div");
   document.body.appendChild(container);
+  root = client.createRoot(container);
 });
 
 afterEach(() => {
-  unmountComponentAtNode(container);
+  act(() => {
+    root.unmount();
+  });
+  root = null;
   container.remove();
   container = null;
 });
@@ -18,7 +24,7 @@ afterEach(() => {
 describe("Dashboard component", () => {
   it("should render some root element", () => {
     act(() => {
-      render(<Dashboard rows={1} columns={1}></Dashboard>, container);
+      root.render(<Dashboard rows={1} columns={1}></Dashboard>);
     });
   });
 });
