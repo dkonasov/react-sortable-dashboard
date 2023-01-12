1900709eb7da8dc8f07ccc02d9a69955e44a7560
Konasov Dmitriy <d.konasov@yandex.ru>
2023-01-13 02:01:25 +0300

    tests: added test for non-zero width/height for dashboard

--- packages/react-sortable-dashboard/e2e/main.e2e-spec.ts
+++ packages/react-sortable-dashboard/e2e/main.e2e-spec.ts
@@ -4,6 +4,7 @@ jest.setTimeout(50000);
 describe("dashboard", () => {
   let browser: Browser;
   const rootElementSelector = "#root";
+  const storybookBaseAddress = `http://127.0.0.1:8080/iframe.html?id=dashboard--default&viewMode=story`;
 
   beforeEach(async () => {
     browser = await launch();
@@ -17,13 +18,10 @@ describe("dashboard", () => {
     const page = await browser.newPage();
     page.setDefaultTimeout(0);
 
-    await page.goto(
-      `http://127.0.0.1:8080/iframe.html?id=dashboard--default&viewMode=story`,
-      {
-        waitUntil: "networkidle2",
-        timeout: 0,
-      }
-    );
+    await page.goto(storybookBaseAddress, {
+      waitUntil: "networkidle2",
+      timeout: 0,
+    });
 
     await page.waitForSelector(rootElementSelector);
     const [containerWidth, containerHeight, dashboardWidth, dashboardHeight] =
@@ -40,5 +38,25 @@ describe("dashboard", () => {
 
     expect(containerWidth).toBe(dashboardWidth);
     expect(containerHeight).toBe(dashboardHeight);
+  });
+
+  it("width and height should be greater than zero", async () => {
+    const page = await browser.newPage();
+    page.setDefaultTimeout(0);
+
+    await page.goto(storybookBaseAddress, {
+      waitUntil: "networkidle2",
+      timeout: 0,
+    });
+
+    await page.waitForSelector(rootElementSelector);
+    const [width, height] = await page.evaluate((rootElementSelector) => {
+      const container = document.querySelector(rootElementSelector);
+      const dashboard = container?.firstElementChild;
+      return [dashboard?.clientWidth ?? 0, dashboard?.clientHeight ?? 0];
+    }, rootElementSelector);
+
+    expect(width).toBeGreaterThan(0);
+    expect(height).toBeGreaterThan(0);
   });
 });
